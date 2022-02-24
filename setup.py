b5dbe5495614e8013fc7a0825ba6b2429972022a
Raiytak <mathieu.salaun12@gmail.com>
2022-02-24 11:43:23 +0100

    Use of requirement

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    requirements = f.read()
+
+list_requirements = requirements.split("\n")
+
 setup(
     name="birthday-calendar",
     version="1.0",
@@ -18,6 +23,6 @@ setup(
     author="Mathieu Salaun",
     author_email="mathieu.salaun12@gmail.com",
     keywords=["birthday", "calendar", "birthday calendar"],
-    install_requires=["googletrans>=4.0.0-rc1"],
+    install_requires=list_requirements,
     packages=["birthday_calendar"],
 )
